03d028ab37e7cb9c005e5bb850c1639218462a64
CodeKhenzy <codingkhenzy@gmail.com>
2022-10-17 15:42:01 +0200

    database, basic project setup

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const typeOrmConnection = require('./src/database/db');
 
 require('dotenv').config(); // Loading in .env file
 
@@ -6,6 +9,44 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] }); // Creating 
 
 client.once('ready', (c) => {
     console.log(`Logged in as ${c.user.tag}!`);
+
+    typeOrmConnection.initialize().then(() => {
+        console.log('Initialized Database!');
+    });
 });
 
-client.login(process.env.TOKEN); // Logging in
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'src/commands');
+const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
+}
+
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+});
+
+client.login(process.env.TOKEN); // Logging in the client
--- src/commands/test.js
+++ src/commands/test.js
@@ -3,6 +3,6 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder().setName('test').setDescription('Test command'),
     async execute(interaction) {
-        await interaction.reply('Test command executed!');
+        await interaction.reply(`Test command executed by: **${interaction.user.tag}**`);
     },
 };
